0a2bdb6b772f09dbf0fe14236253da4229e3aa1a
deirdreOH <dohalloran@wit.ie>
2025-09-18 12:50:50 +0100

    Added keys to tasks

--- tasky/src/App.jsx
+++ tasky/src/App.jsx
@@ -4,36 +4,38 @@ import "./App.css";
 import Task from "./components/Task";
 
 function App() {
-
   const [taskState, setTaskState] = useState({
     tasks: [
-      { title: "Dishes", description: "Empty dishwasher", deadline: "Today" },
       {
+        id: 1,
+        title: "Dishes",
+        description: "Empty dishwasher",
+        deadline: "Today",
+      },
+      {
+        id: 2,
         title: "Laundry",
         description: "Fold clothes and put away",
         deadline: "Tomorrow",
       },
-      { title: "Tidy up", deadline: "Today" },
+      { id: 3, title: "Tidy up", deadline: "Today" },
     ],
   });
 
+  return (
+    <div className="container">
+      <h1>Tasky</h1>
 
-    return (
-      <div className="container">
-        <h1>Tasky</h1>
-        
-        {taskState.tasks.map((task) => (
-          <Task
-            title={task.title}
-            description={task.description}
-            deadline={task.deadline}
-          />
-        ))}
-
-
-      </div>
-    );
-
+      {taskState.tasks.map((task) => (
+        <Task
+          title={task.title}
+          description={task.description}
+          deadline={task.deadline}
+          key={task.id}
+        />
+      ))}
+    </div>
+  );
 }
 
 export default App;
